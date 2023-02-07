c7422539b6547ef274bc3e69f84944201322966d
se030 <se030.kim@gmail.com>
2023-02-07 11:52:50 +0900

    feat: component class enhancement
    - setObservers method to observe/unobserve global storage
    - initialize observer list at registerObservers()
    - all set-ups on the actual element done at connectedCallback()
    - utilize element attributes to observe state change using observedAttributes(), attributesChangedCallback()

--- src/core/Component.mjs
+++ src/core/Component.mjs
@@ -1,14 +1,15 @@
+import { observe, unobserve } from './flux/observable.mjs';
+
 export default class Component extends HTMLElement {
   state;
   handlers;
 
-  constructor(props = {}) {
+  constructor() {
     super();
-    this.state = { ...props };
-    this.handlers = {};
 
-    this.setObservers();
-    this.setHandlers();
+    this.state = {};
+    this.observers = [];
+    this.registerObservers();
   }
 
   setState(newState) {
@@ -16,12 +17,17 @@ export default class Component extends HTMLElement {
     this.render();
   }
 
-  setObservers() {
-    // call observe() to subscribe store states
-  }
-
   setHandlers() {
     // apply handlers
+  }
+
+  registerObservers() {
+    // initialize this.observers
+  }
+
+  setObservers() {
+    // observe or unobserve store states
+    this.observers.forEach((handler) => (this.isConnected ? observe(handler) : unobserve(handler)));
   }
 
   connectedCallback() {
@@ -33,4 +39,24 @@ export default class Component extends HTMLElement {
   }
 
   template() {}
+
+  connectedCallback() {
+    this.setHandlers();
+    this.setObservers();
+    this.render();
+  }
+
+  static get observedAttributes() {
+    // state-attributes array
+    return [];
+  }
+
+  attributeChangedCallback(_, oldValue, newValue) {
+    if (oldValue === newValue) return;
+    this.render();
+  }
+
+  disconnectedCallback() {
+    this.setObservers();
+  }
 }
