28923d7c83654e23134ae168f6919ec67de920a9
se030 <se030.kim@gmail.com>
2023-02-07 11:54:48 +0900

    refactor

--- src/App.mjs
+++ src/App.mjs
@@ -1,5 +1,13 @@
 import Component from './core/Component.mjs';
 
+import StateDiv from './components/StateDiv.mjs';
+import HiButton from './components/HiButton.mjs';
+import ByeButton from './components/ByeButton.mjs';
+
+customElements.define('state-div', StateDiv);
+customElements.define('hi-button', HiButton);
+customElements.define('bye-button', ByeButton);
+
 export default class App extends Component {
   template() {
     return `
--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
 import App from './App.mjs';
-import StateDiv from './components/StateDiv.mjs';
-import HiButton from './components/HiButton.mjs';
-import ByeButton from './components/ByeButton.mjs';
 
 customElements.define('my-app', App);
-
-customElements.define('state-div', StateDiv);
-customElements.define('hi-button', HiButton);
-customElements.define('bye-button', ByeButton);
